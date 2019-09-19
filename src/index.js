a54db5c9adf94c24b0894d9e6483ebc2c476f62e
Bernhard Essl <bernhardessl@aeoontechnologies.com>
2019-09-19 16:54:38 +0200

    postgres and dotenv

--- src/index.js
+++ src/index.js
@@ -5,6 +5,25 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const jwt = require('express-jwt');
 const jwksRsa = require('jwks-rsa');
+const { Pool } = require('pg');
+const dotenv = require('dotenv').config();
+
+if (dotenv.error) {
+    console.log('No .env config found!');
+    throw dotenv.error
+}
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+
+pool.on('connect', () => {
+    console.log('connected to the db');
+});
 
 const app = express();
 app.use(helmet());
@@ -17,10 +36,10 @@ const checkJwt = jwt({
         cache: true,
         rateLimit: true,
         jwksRequestsPerMinute: 5,
-        jwksUri: 'https://gymbuddy.eu.auth0.com/.well-known/jwks.json'
+        jwksUri: `${process.env.AUTH0_ISSUER}.well-known/jwks.json`
     }),
-    audience: 'https://gymbuddy.com',
-    issuer: 'https://gymbuddy.eu.auth0.com/',
+    audience: process.env.AUTH0_AUDIENCE,
+    issuer: process.env.AUTH0_ISSUER,
     algorithms: ['RS256']
 });
 app.use(checkJwt);
@@ -28,6 +47,16 @@ app.use(checkJwt);
 app.get('/',(req, res) => {
     res.send('Hello world');
 });
+
+app.get('/api/v1/exercises/', (req, res) => {
+    pool.query('SELECT * FROM exercise ORDER BY title', (error, results) => {
+        if (error) {
+            throw error
+        }
+        res.status(200).json(results.rows)
+    });
+});
+
 
 app.listen(3001, () => {
     console.log('listening on port 3001');
