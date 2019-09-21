0e8bef25ae0ad30ae675bc488c5aab05cccbeaa3
Bernhard Essl <bernhardessl@aeoontechnologies.com>
2019-09-21 11:49:15 +0200

    test for returning detail exercise route

--- src/api.test.js
+++ src/api.test.js
@@ -11,12 +11,8 @@ before(async () => {
     const pool = new Pool({
         connectionString:  'postgresql://postgres:postgres@postgres:5432/gymbuddy'
     });
-    await pool.query('DROP DATABASE IF EXISTS gymbuddy_test').then(e=>{
-        console.log('++++++++ droped /////');
-    });
-    await pool.query('CREATE DATABASE gymbuddy_test;').then(e=>{
-        console.log('++++++++ created ////');
-    });
+    await pool.query('DROP DATABASE IF EXISTS gymbuddy_test');
+    await pool.query('CREATE DATABASE gymbuddy_test;');
     await knex.migrate.latest()
         .then(async function() {
             return await knex.seed.run();
@@ -28,7 +24,7 @@ before(async () => {
 
 describe('API', function () {
 
-    it('GET /ping health status check', function (done) {
+    it('Health status check', function (done) {
         request(app)
             .get('/ping')
             .set('Accept', 'application/json')
@@ -39,7 +35,7 @@ describe('API', function () {
                 },done);
     });
 
-    it('GET /api/v1/exercises/by_day/1 Return all exercises for day 1', function (done) {
+    it('Return all exercises for day 1', function (done) {
         request(app)
             .get('/api/v1/exercises/by_day/1')
             .set('Accept', 'application/json')
@@ -50,6 +46,23 @@ describe('API', function () {
                 expect(r.body[0].id).to.be.a('number');
                 expect(r.body[1].title).to.be.a('string');
                 expect(r.body[2].img_url).to.be.a('string');
+            })
+            .expect(200, done);
+    });
+
+    it('Return one exercise instance by exercise ID', function (done) {
+        request(app)
+            .get('/api/v1/exercises/1')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(r => {
+                expect(r.body).to.be.a('array');
+                expect(r.body).to.have.lengthOf(1);
+                expect(r.body[0].id).to.be.a('number');
+                expect(r.body[0].title).to.be.a('string');
+                expect(r.body[0].img_url).to.be.a('string');
+                expect(r.body[0].id).to.equal(1);
+                expect(r.body[0].title).to.equal('Lower back bench');
             })
             .expect(200, done);
     });
